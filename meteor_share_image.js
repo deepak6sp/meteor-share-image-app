1308f6ad04b3b58c0ee6478f8e91ec759baf5f26
deepak6sp <deepak6sp@gmail.com>
2015-12-30 22:59:47 +1100

    ratings aded

--- meteor_share_image.js
+++ meteor_share_image.js
@@ -1,23 +1,41 @@
+Images = new Mongo.Collection("images");
+//console.log(Images.find().count());
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.image_show.helpers({
+    images:Images.find()
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.image_show.events({
+    'click .delete-btn' :function(){
+      var image_id = this._id;
+      $("#"+image_id).hide('slow',function(){
+        Images.remove({"_id":image_id})
+      });   
+    },
+    'click .rate-image' : function(event){
+      var rating = $(event.currentTarget).data('userrating');
+      console.log("rating is "+rating);
+    } 
+    
   });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+
+  if (Images.find().count() == 0){
+    Meteor.startup(function () {
+    for (var i=1;i<23;i++){
+      Images.insert ({
+        img_src : "img_"+i+".jpg",
+        img_alt : "this is img"+i+".jpg"
+      });
+    }
   });
-}
+  }  
+
+} 
+ 
